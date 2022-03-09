22959cfdc32c6a7609c425b08c003254a1d2aea3
Micah Villaruz <micahmvillaruz@gmail.com>
2022-03-09 15:22:27 +0800

    Final Version

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Root_Password1@localhost/api_final_proj"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root@localhost/api_final_proj"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
